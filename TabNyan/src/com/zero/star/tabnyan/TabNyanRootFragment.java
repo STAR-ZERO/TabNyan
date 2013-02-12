6ab34d94f0cf19e06be030ee59e08283c612423a
STAR_ZERO <kenji.01.star@gmail.com>
2013-02-13 00:09:39 +0900

    Call child fragment onActivityResult method

--- TabNyan/src/com/zero/star/tabnyan/TabNyanRootFragment.java
+++ TabNyan/src/com/zero/star/tabnyan/TabNyanRootFragment.java
@@ -1,5 +1,6 @@
 package com.zero.star.tabnyan;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -10,35 +11,35 @@ import android.view.ViewGroup;
 
 public class TabNyanRootFragment extends Fragment {
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.fragment_tabnyan_root, container, false);
-	}
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_tabnyan_root, container, false);
+    }
 
-	@Override
-	public void onActivityCreated(Bundle savedInstanceState) {
-		super.onActivityCreated(savedInstanceState);
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
 
         initFragment();
-	}
+    }
 
-	/**
-	 * Pop fragment backstack.
-	 * @return return true if exists backstack
-	 */
-	public boolean popBackStack() {
-		return getChildFragmentManager().popBackStackImmediate();
-	}
+    /**
+     * Pop fragment backstack.
+     * @return return true if exists backstack
+     */
+    public boolean popBackStack() {
+        return getChildFragmentManager().popBackStackImmediate();
+    }
 
-	/**
-	 * Initialize Fragment.
-	 */
-	private void initFragment() {
+    /**
+     * Initialize Fragment.
+     */
+    private void initFragment() {
 
-		Bundle args = getArguments();
-		String rootClass = args.getString(TabNyanActivity.ROOT_FRAGMENT_ARGS);
+        Bundle args = getArguments();
+        String rootClass = args.getString(TabNyanActivity.ROOT_FRAGMENT_ARGS);
 
-		FragmentManager fragmentManager = getChildFragmentManager();
+        FragmentManager fragmentManager = getChildFragmentManager();
         if (fragmentManager.findFragmentById(R.id.fragment) != null) {
             return;
         }
@@ -49,15 +50,22 @@ public class TabNyanRootFragment extends Fragment {
         fragmentTransaction.replace(R.id.fragment, fragment);
 
         fragmentTransaction.commit();
-	}
+    }
 
-	/**
-	 * Return Fragment in displayed
-	 * @return Fragment
-	 */
-	Fragment getCurrentFragment() {
-	    FragmentManager fragmentManager = getChildFragmentManager();
-	    return fragmentManager.findFragmentById(R.id.fragment);
-	}
+    /**
+     * Return Fragment in displayed
+     * @return Fragment
+     */
+    Fragment getCurrentFragment() {
+        FragmentManager fragmentManager = getChildFragmentManager();
+        return fragmentManager.findFragmentById(R.id.fragment);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Call child fragment method
+        getCurrentFragment().onActivityResult(requestCode, resultCode, data);
+    }
 
 }
